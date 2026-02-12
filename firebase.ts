f87e3b7b4026a0508695db9c2ce126060d746634
Mohit <mohitrocks001@gmail.com>
2026-02-12 01:05:02 -0800

    Fix Firebase setup

--- firebase.ts
+++ firebase.ts
@@ -18,5 +18,22 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2NpCJYpkMWK6wk47LEKInkR-nAu3lbh8",
+  authDomain: "picvote-1150a.firebaseapp.com",
+  projectId: "picvote-1150a",
+  storageBucket: "picvote-1150a.firebasestorage.app",
+  messagingSenderId: "783966047062",
+  appId: "1:783966047062:web:9ceac5aa3b208f81eb24f7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export const auth = getAuth(app);
