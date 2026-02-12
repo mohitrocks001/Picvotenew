95bde7772327da0a8f45e7e6a96a0b110ca03529
Mohit <mohitrocks001@gmail.com>
2026-02-12 00:56:12 -0800

    Fix Firebase setup

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyB2NpCJYpkMWK6wk47LEKInkR-nAu3lbh8",
